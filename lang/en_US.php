77ae887f405156700204b7b0f8db6050ebcc71fd
Andrew Short <andrewjshort@gmail.com>
2010-02-14 19:09:48 +1100

    MINOR: Added generated language files.

---
+++ lang/en_US.php
@@ -0,0 +1,78 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['MemberProfileField']['PLURALNAME'] = array(
+	'Member Profile Fields',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['MemberProfileField']['SINGULARNAME'] = array(
+	'Member Profile Field',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['MemberProfilePage']['PLURALNAME'] = array(
+	'Member Profile Pags',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['MemberProfilePage']['SINGULARNAME'] = array(
+	'Member Profile Page',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['MemberProfiles']['AFTERRED'] = 'After Registration';
+$lang['en_US']['MemberProfiles']['ALLOWREG'] = 'Allow registration via this page';
+$lang['en_US']['MemberProfiles']['CANNOTCONFIRMLOGGEDIN'] = 'You cannot confirm account while you are logged in.';
+$lang['en_US']['MemberProfiles']['CANNOTREGPLEASELOGIN'] = 'You cannot register on this profile page. Please login to edit your profile.';
+$lang['en_US']['MemberProfiles']['CONFIRMCONTENT'] = 'Confirmation Content';
+$lang['en_US']['MemberProfiles']['CONFIRMNOTE'] = 'This content is displayed when a user confirms their account.';
+$lang['en_US']['MemberProfiles']['CONTENT'] = 'Content';
+$lang['en_US']['MemberProfiles']['DEFAULTVALUE'] = 'Default Value';
+$lang['en_US']['MemberProfiles']['EMAILCONFIRMATION'] = 'Email Confirmation';
+$lang['en_US']['MemberProfiles']['EMAILFROM'] = 'Email from';
+$lang['en_US']['MemberProfiles']['EMAILTEMPLATE'] = 'Email template';
+$lang['en_US']['MemberProfiles']['EMAILVALID'] = 'Require email validation';
+$lang['en_US']['MemberProfiles']['EMAILVALIDATION'] = 'Email Validation';
+$lang['en_US']['MemberProfiles']['FIELDOPTIONS'] = 'Field Options';
+$lang['en_US']['MemberProfiles']['GROUPSETTINGS'] = 'Group Settings';
+$lang['en_US']['MemberProfiles']['GROUPSNOTE'] = '<p>Any users registering via this page will be added to the below groups (if registration is enabled). Conversely, a member must belong to these groups in order to edit their profile on this page.</p>';
+$lang['en_US']['MemberProfiles']['LOGIN'] = 'If you already have an account you can <a href="%s">log in here</a>.';
+$lang['en_US']['MemberProfiles']['MANUALLYCONFIRM'] = 'Manually confirm';
+$lang['en_US']['MemberProfiles']['MEMBERFIELD'] = 'Member Field';
+$lang['en_US']['MemberProfiles']['MEMBERWITHSAME'] = 'There is already a member with the same %s.';
+$lang['en_US']['MemberProfiles']['NEEDSVALIDATIONTOLOGIN'] = 'You must validate your account before you can log in.';
+$lang['en_US']['MemberProfiles']['NOLOGINTILLCONFIRMED'] = 'The member cannot log in until their account is confirmed.';
+$lang['en_US']['MemberProfiles']['PAGESETTINGS'] = 'Page Settings';
+$lang['en_US']['MemberProfiles']['PROFILE'] = 'Profile';
+$lang['en_US']['MemberProfiles']['PROFILEREGFIELDS'] = 'Profile/Registration Fields';
+$lang['en_US']['MemberProfiles']['PROFILEUPDATED'] = 'Your profile has been updated.';
+$lang['en_US']['MemberProfiles']['REGISTER'] = 'Register';
+$lang['en_US']['MemberProfiles']['REGISTRATION'] = 'Registration';
+$lang['en_US']['MemberProfiles']['REGSETTINGS'] = 'Registration Settings';
+$lang['en_US']['MemberProfiles']['RESEND'] = 'Resend confirmation email';
+$lang['en_US']['MemberProfiles']['SAVE'] = 'Save';
+$lang['en_US']['MemberProfiles']['TITLE'] = 'Title';
+$lang['en_US']['MemberProfiles']['UNCONFIRMED'] = 'Unconfirmed';
+$lang['en_US']['MemberProfiles']['VALIDATION'] = 'Validation';
+$lang['en_US']['MemberProfiles']['VALIDEMAILSUBJECT'] = 'Validation email subject';
+$lang['en_US']['MemberProfiles']['VALIDOPTIONS'] = 'Validation Options';
+$lang['en_US']['OrderableCTF.ss']['ADDITEM'] = array(
+	'Add %s',
+	PR_MEDIUM,
+	'Add [name]'
+);
+$lang['en_US']['OrderableCTF.ss']['CSVEXPORT'] = 'Export to CSV';
+$lang['en_US']['OrderableCTF.ss']['NOITEMSFOUND'] = 'No items found';
+$lang['en_US']['OrderableCTF.ss']['SORTASC'] = 'Sort ascending';
+$lang['en_US']['OrderableCTF.ss']['SORTDESC'] = 'Sort descending';
+$lang['en_US']['TableListField_PageControls.ss']['DISPLAYING'] = 'Displaying';
+$lang['en_US']['TableListField_PageControls.ss']['OF'] = 'of';
+$lang['en_US']['TableListField_PageControls.ss']['TO'] = 'to';
+$lang['en_US']['TableListField_PageControls.ss']['VIEWFIRST'] = 'View first';
+$lang['en_US']['TableListField_PageControls.ss']['VIEWLAST'] = 'View last';
+$lang['en_US']['TableListField_PageControls.ss']['VIEWNEXT'] = 'View next';
+$lang['en_US']['TableListField_PageControls.ss']['VIEWPREVIOUS'] = 'View previous';
+
+?>
