bb1687f66c133ce8a4ac8386de29e7943ab4ebe2
Andrew Short <andrewjshort@gmail.com>
2012-03-06 19:07:48 +1100

    Made the orderable dependency error more prominent and less ambiguous. Closes #23.

--- _config.php
+++ _config.php
@@ -3,8 +3,20 @@
  * @package silverstripe-memberprofiles
  */
 
-if (!class_exists('Orderable')) {
-	throw new Exception('The Member Profiles module requires the Orderable module.');
+if(!ClassInfo::exists('Orderaable')) {
+	$view = new DebugView();
+	$link = 'https://github.com/ajshort/silverstripe-orderable';
+
+	if(!headers_sent()) {
+		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
+	}
+
+	$view->writeHeader();
+	$view->writeInfo('Dependency Error', 'The Member Profiles module requires the Orderable module.');
+	$view->writeParagraph("Please install the <a href=\"$link\">Orderable</a> module.");
+	$view->writeFooter();
+
+	exit;
 }
 
 Director::addRules(20, array(
